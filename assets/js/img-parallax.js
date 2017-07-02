829ec67e14383366bdcbbe4876d2be35ffd9689c
Asmaodeus <cadoz.olivier@gmail.com>
2017-07-02 11:44:09 +0200

    improve parallax

--- assets/js/img-parallax.js
+++ assets/js/img-parallax.js
@@ -1,13 +1,13 @@
 $(function(){
 
-  // $window.on('scroll', function(){
-  //   if ($window.width() >= 768) {
-  //     $('.js-img-parallax').each(function(){
-  //       if (ws >= ($(this).offset().top - 250)) {
-  //         position = (ws - $(this).offset().top)/10*1.5;
-  //         $(this).css({'background-position': 'center -' + position + 'px'});
-  //       }
-  //     });
-  //   }
-  // });
+  $window.on('scroll', function(){
+    if ($window.width() >= 768) {
+      $('.js-img-parallax').each(function(){
+        if (ws + sh >= ($(this).offset().top - 250)) {
+          position = (ws + sh - $(this).offset().top)/10*0.5;
+          $(this).css({'background-position': 'center ' + position + 'px'});
+        }
+      });
+    }
+  });
 });
